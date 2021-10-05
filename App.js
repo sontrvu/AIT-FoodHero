5874bdad67fb297a9fe06a3469ac0469d0fb3a75
LAPTOP-9JGJEGCK\Simo <simo.quircio@hotmail.com>
2021-10-05 22:03:54 +1100

    new feature ahahahah

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
