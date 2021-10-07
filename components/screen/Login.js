fe4f07cb3fca0da982bf4fc4221dbb740d5808c5
LAPTOP-9JGJEGCK\Simo <simo.quircio@hotmail.com>
2021-10-07 18:04:50 +1100

    Create login Screen

---
+++ components/screen/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+import LoginStyle from '../view/LoginStyle';
+
+function Login(props) {
+    return (
+        <View>
+            <LoginStyle/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default Login;
