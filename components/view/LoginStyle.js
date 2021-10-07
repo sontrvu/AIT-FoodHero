a62ddd4ee5bbe4ff463ee1ede2560b9970a7ec10
Harry Vu <harry.vu@tomsoft.com.au>
2021-10-07 19:53:06 +1100

    Fix stuff

--- components/view/LoginStyle.js
+++ components/view/LoginStyle.js
@@ -7,23 +7,21 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 function LoginStyle(props) {
     return (
         <View>
-            <View>
-                <View style={styles.headerContainer}>
-                    <Text style={styles.loginTextHeader}>Login</Text>
-                    <Text style={styles.loginText}>Please sign in to continue</Text>
-                </View>
-                    
-                <View style={styles.bodyContainer}>
-                    <Text style={styles.text}>Email</Text>
-                    <TextInput style={styles.input} placeholder="Enter your email"></TextInput>
-                    <Text style={styles.text}>Password</Text>
-                    <TextInput style={styles.input} placeholder="Enter your password"></TextInput>
-
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Login  <FontAwesome5 name="arrow-alt-circle-right" size={24} color="black" /></Text>
-                    </TouchableOpacity>
-                </View>     
+            <View style={styles.headerContainer}>
+                <Text style={styles.loginTextHeader}>Login</Text>
+                <Text style={styles.loginText}>Please sign in to continue</Text>
             </View>
+                
+            <View style={styles.bodyContainer}>
+                <Text style={styles.text}>Email</Text>
+                <TextInput style={styles.input} placeholder="Enter your email"></TextInput>
+                <Text style={styles.text}>Password</Text>
+                <TextInput style={styles.input} placeholder="Enter your password"></TextInput>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Login  <FontAwesome5 name="arrow-alt-circle-right" size={24} color="black" /></Text>
+                </TouchableOpacity>
+            </View>     
 
 
 
