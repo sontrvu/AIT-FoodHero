366a79764954396a5d983416bceeb86fc174f9b4
LAPTOP-9JGJEGCK\Simo <simo.quircio@hotmail.com>
2021-10-07 18:03:45 +1100

    Just swapped with the login screen to visualize it on expo

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 
 import MainScreen from './components/screen/Main'
+import LoginScreen from './components/screen/Login'
+import { Alert, Button, View } from 'react-native';
+
 
 const Stack = createStackNavigator();
 
@@ -11,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
        <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
           
        </Stack.Navigator>
     </NavigationContainer>
