cea95e28fa1304e611a190aaa99659540f11b3b5
Harry Vu <harry.vu@tomsoft.com.au>
2021-10-13 18:07:16 +1100

    Improve Main

--- components/screen/Main.js
+++ components/screen/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View } from 'react-native';
 import * as AppConstant from '../../helpers/appConstant';
 
 import MainHeader from '../view/MainHeader';
@@ -8,13 +8,29 @@ import RestaurantList from '../view/RestaurantList';
 function Main(props) {
   return (
     <View>
-      <MainHeader />
-      <RestaurantList title={'New Restaurents'} />
-      <RestaurantList title={'Random'} />
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.scrollViewContentContainer}
+        stickyHeaderIndices={[0]}
+        bounces={false}
+      >
+        {/* Sticky header */}
+        <MainHeader />
+
+        {/* Restaurent list */}
+        <RestaurantList title={'New Restaurents'} />
+        <RestaurantList title={'Random'} />
+        <RestaurantList title={'Hey hey'} />
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollView: {},
+  scrollViewContentContainer: {
+    paddingBottom: 30,
+  },
+});
 
 export default Main;
--- components/view/MainHeader.js
+++ components/view/MainHeader.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import * as AppConstant from '../../helpers/appConstant';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 
 function MainHeader(props) {
   return (
     <View style={styles.topBar}>
       <View style={{ flexDirection: 'row' }}>
+        {/* User address */}
         <View style={{ flex: 1 }}>
-          <Text style={{ color: 'white', fontSize: 15 }}>Address</Text>
-          <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>
-            20 Spencer St
-          </Text>
+          <Text style={styles.addressLabel}>Address</Text>
+          <Text style={styles.addressDetail}>20 Spencer St</Text>
         </View>
-        <View
-          style={{ flex: 1, alignItems: 'flex-end', justifyContent: 'center' }}
-        >
-          <FontAwesome5 name="user-circle" size={35} color="white" />
+
+        {/* Profile button */}
+        <View style={styles.profileIconContainer}>
+          <FontAwesome5
+            name="user-circle"
+            size={35}
+            color={AppConstant.COLOR_SECONDARY}
+          />
         </View>
       </View>
-      <TextInput style={styles.searchBar}></TextInput>
+
+      {/* Search bar */}
+      <View style={styles.searchBarContainer}>
+        <View style={styles.searchIconContainer}>
+          <Ionicons name="search-outline" size={25} color="gray" />
+        </View>
+        <TextInput
+          style={styles.searchBar}
+          placeholder={'Restaurents, fast food, cuisine...'}
+        />
+      </View>
     </View>
   );
 }
@@ -30,15 +43,41 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     paddingHorizontal: 15,
     backgroundColor: AppConstant.COLOR_PRIMARY,
-    // height: 150,
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
   },
-  searchBar: {
+  addressLabel: {
+    fontSize: 13,
+    color: AppConstant.COLOR_SECONDARY,
+  },
+  addressDetail: {
+    marginTop: 5,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: AppConstant.COLOR_SECONDARY,
+  },
+  profileIconContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  searchBarContainer: {
     marginTop: 15,
-    height: 50,
+    height: 40,
     backgroundColor: 'white',
     borderRadius: 30,
+    flexDirection: 'row',
+  },
+  searchIconContainer: {
+    width: 40,
+    marginLeft: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchBar: {
+    flex: 1,
+    fontWeight: '500',
+    fontSize: 18,
   },
 });
 
