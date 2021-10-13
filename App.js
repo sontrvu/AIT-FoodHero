b74f9b303c39a4ed01e8a5c3d4152319001a5247
Harry Vu <harry.vu@tomsoft.com.au>
2021-10-13 17:29:36 +1100

    Add Restaurent Detail to stack

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 
-import MainScreen from './components/screen/Main'
-import LoginScreen from './components/screen/Login'
-import SignupScreen from './components/screen/Signup'
-import SplashScreen from './components/screen/Splash'
-import CheckoutScreen from './components/screen/Checkout'
+import MainScreen from './components/screen/Main';
+import LoginScreen from './components/screen/Login';
+import SignupScreen from './components/screen/Signup';
+import SplashScreen from './components/screen/Splash';
+import CheckoutScreen from './components/screen/Checkout';
+import RestaurantDetail from './components/screen/RestaurantDetail';
 
 //import HomeScreen from './components/screen/Home'
 
@@ -16,15 +17,39 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName="Splash">
-          <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Checkout" component={CheckoutScreen} options={{ headerShown: false }}/>
-       </Stack.Navigator>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RestaurantDetail"
+          component={RestaurantDetail}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Checkout"
+          component={CheckoutScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
-
